2a9a13da9ad5e5dacc06b9608d89ec58468ae0b9
Alexgbp <bboy91@bk.ru>
2024-05-01 20:43:54 +0300

    работа с плавностью модалки

--- src/components/ListUserComponent/ListUserComponent.jsx
+++ src/components/ListUserComponent/ListUserComponent.jsx
@@ -7,11 +7,15 @@ export function ListUserComponent({users, isVisible, setIsVisible,  isShowLink, 
     setIsShowLink(!isShowLink)
     setIsVisible(!isVisible)
   }
- 
+
+  const hideLinkOnUserPage =() => {
+    setIsShowLink(!isShowLink)
+    setIsVisible(!isVisible)
+  }
 
   return (
     <ul className={styles.list_block}>
-      <ModalComponent isVisible={isShowLink} />
+      <ModalComponent onClick={hideLinkOnUserPage} isVisible={isShowLink} />
       {users?.map((user) => {
         return <li onClick={showLinkOnUserPage} className={styles.list_block__element} key={user.id}>Пользователь <span className={styles.user_name}>{user.login}</span> имеет {user.repoCount} репозиториев</li>
       })}
--- src/components/ModalComponent/ModalComponent.jsx
+++ src/components/ModalComponent/ModalComponent.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import styles from "../ModalComponent/ModalComponent.module.css";
 
-export function ModalComponent({isVisible}) {
+export function ModalComponent({isVisible, onClick}) {
   return (
     <>
       <div className={`${styles.modal_block} ${isVisible ? styles.show : ""}`}>
-      <div className={styles.icon_close}>X</div>
+      <div onClick={onClick} className={styles.icon_close}>X</div>
         ModalComponent
         </div>
     </>
